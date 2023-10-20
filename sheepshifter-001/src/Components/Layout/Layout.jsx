90f86f136c7cbaeabc2fbecd08716d7f2c9c9600
BadBeatBuck <matt.hill.email@gmail.com>
2023-10-20 09:05:18 -0700

    import raw images

--- sheepshifter-001/src/Components/Layout/Layout.jsx
+++ sheepshifter-001/src/Components/Layout/Layout.jsx
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TestFabric7 from "../TestFabric7/TestFabric7";
 import css from "./Layout.module.css";
 
-import tay001 from "./tay001.jpg";
-import tay003 from "./tay003.jpg";
+const rawImages = [
+  "./tay001.jpg",
+  "./tay003.jpg",
+  "./tay003.jpg",
+  "./tay003.jpg",
+  "./tay003.jpg",
+];
 
 function Layout({ data }) {
+  const [images, setImages] = useState({});
+
+  useEffect(() => {
+    async function loadImages() {
+      const loadedImages = {};
+
+      for (const imagePath of rawImages) {
+        const module = await import(imagePath);
+        loadedImages[imagePath] = module.default;
+      }
+
+      setImages(loadedImages);
+    }
+
+    loadImages();
+  }, []);
+
+  const renderedImages = rawImages.map((imagePath, index) => (
+    <div key={imagePath + index} className={css.box}>
+      <img src={images[imagePath]} alt={imagePath} />
+    </div>
+  ));
+
   return (
     <div className={css.main}>
-      <div className={css.topRow}>
-        <div className={css.box}>
-          <img src={tay001} alt="tay001" />
-        </div>
-        <div className={css.box}>
-          <img src={tay003} alt="tay003" />
-        </div>
-        <div className={css.box}>
-          <img src={tay001} alt="tay001" />
-        </div>
-        <div className={css.box}>
-          <img src={tay003} alt="tay003" />
-        </div>
-        <div className={css.box}>
-          <img src={tay003} alt="tay003" />
-        </div>
-      </div>
+      <div className={css.topRow}>{renderedImages}</div>
       <div className={css.bottomRow}>
         <div className={css.tallBox}></div>
         <div className={css.largeBox}>
