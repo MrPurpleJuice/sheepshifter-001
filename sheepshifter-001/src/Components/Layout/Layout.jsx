55f448d726a3f147c65795ef9dcd5d6c8d8019ba
BadBeatBuck <matt.hill.email@gmail.com>
2023-10-20 01:43:26 -0700

    add top images

--- sheepshifter-001/src/Components/Layout/Layout.jsx
+++ sheepshifter-001/src/Components/Layout/Layout.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import TestFabric7 from "../TestFabric7/TestFabric7";
 import css from "./Layout.module.css";
 
+import tay001 from "./tay001.jpg";
+import tay002 from "./tay002.jpg";
+import tay003 from "./tay003.jpg";
+
 function Layout({ data }) {
   return (
     <div className={css.main}>
       <div className={css.topRow}>
-        <div className={css.box}></div>
-        <div className={css.box}></div>
-        <div className={css.box}></div>
-        <div className={css.box}></div>
-        <div className={css.box}></div>
+        <div className={css.box}>
+          <img src={tay001} alt="tay001" />
+        </div>
+        <div className={css.box}>
+          <img src={tay002} alt="tay002" />
+        </div>
+        <div className={css.box}>
+          <img src={tay003} alt="tay003" />
+        </div>
+        <div className={css.box}>
+          <img src={tay003} alt="tay003" />
+        </div>
+        <div className={css.box}>
+          <img src={tay003} alt="tay003" />
+        </div>
       </div>
       <div className={css.bottomRow}>
         <div className={css.tallBox}></div>
