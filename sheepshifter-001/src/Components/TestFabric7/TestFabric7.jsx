6e2a3e143192917deacfe054b77e23e16f013189
BadBeatBuck <matt.hill.email@gmail.com>
2023-10-19 22:10:04 -0700

    background works

--- sheepshifter-001/src/Components/TestFabric7/TestFabric7.jsx
+++ sheepshifter-001/src/Components/TestFabric7/TestFabric7.jsx
@@ -34,16 +34,16 @@ export default function TestFabric({ data }) {
 
     const backUrl = "src/assets/test-3-bg.png";
 
-    // fabric.Image.fromURL(backUrl, function (img) {
-    //   // Add background image
-    //   editor?.canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-    //     // scaleX: canvas.width / img.width,
-    //     // scaleY: canvas.height / img.height,
-    //   });
-    // });
-    // fabric.Image.fromURL(backUrl, function (img) {
-    //   editor?.canvas.add(img);
-    // });
+    fabric.Image.fromURL(backUrl, function (img) {
+      // Add background image
+      editor?.canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+        // scaleX: canvas.width / img.width,
+        // scaleY: canvas.height / img.height,
+      });
+    });
+    fabric.Image.fromURL(backUrl, function (img) {
+      editor?.canvas.add(img);
+    });
 
     console.log(`localData.image_urls`, localData.image_urls);
     // canvas.renderAll(); // Re-render the canvas to apply new dimensions
@@ -51,14 +51,13 @@ export default function TestFabric({ data }) {
     for (let i = 0; i < localData.image_urls.length; i++) {
       let imageUrl = localData.image_urls[i];
       let placement = localData.placement_data[i];
-      console.log(`placement`, placement);
-      console.log(`imageUrl`, imageUrl);
 
       fabric.Image.fromURL(imageUrl, function (oImg) {
         oImg.set({
           left: placement.left,
           top: placement.top,
         });
+        oImg.set({ id: "fabric-object-" + i });
         editor?.canvas.add(oImg);
       });
 
